965a6c4bb9c7759c1d69e5e88b85e2bb1b1fe5b5
Anders <anders.gardenas@gmail.com>
2015-10-16 17:11:56 +0200

    added task master

--- work.py
+++ work.py
@@ -1,33 +1,37 @@
 import json
 import os
-
-
-skip = 0
-words = {'han': 0, 'hon' : 0, 'den' : 0,'det' : 0, 'denna': 0, 'denne' : 0, 'hen' : 0}
-
-#config = {'tenant_name':os.environ['ACC-Course'],
-#          'authurl':os.environ['http://smog.uppmax.uu.se:5000/v2.0']}
-
-
 import urllib2
-response = urllib2.urlopen('http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_0.txt')
-#html = response.read()
 
-#with open(html) as f:
-for row in response:
-    if (row == ''):
-        break 
-    if (row == '\n'):
-        continue 
-    json2 = json.loads(row)
-    if ('retweeted_status'  in json2):
-        skip = skip + 1
-        continue
-    text = json2['text']
-    for word in words:
-        n = text.count(word)
-        amount = words.get(word)
-        words[word] = n + amount
-for word in words:
-    print('ordet ' + word + " antalet " + str(words[word]))  
-print('skip ' + str(skip))     
+app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@130.238.29.173/rabbithost')
+
+@app.task
+def calculate (adresses):
+    for adress in adresses:
+        print 'started'
+        skip = 0
+        words = {'han': 0, 'hon' : 0, 'den' : 0,'det' : 0, 'denna': 0, 'denne' : 0, 'hen' : 0}
+
+        response = urllib2.urlopen('http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets/' + adress)
+
+
+        for row in response:
+            if (row == ''):
+                break 
+            if (row == '\n'):
+                continue 
+
+            json2 = json.loads(row)
+            if ('retweeted_status'  in json2):
+                skip = skip + 1
+                continue
+            text = json2['text']
+
+            for word in words:
+                n = text.count(word)
+                amount = words.get(word)
+                words[word] = n + amount
+
+
+            for word in words:
+                print('ordet ' + word + " antalet " + str(words[word]))  
+                print('skip ' + str(skip))     
