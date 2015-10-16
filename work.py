6552217beccc823632b9aab4274f970c99ca179e
Anders <anders.gardenas@gmail.com>
2015-10-16 16:56:53 +0200

    1

--- work.py
+++ work.py
@@ -11,23 +11,23 @@ words = {'han': 0, 'hon' : 0, 'den' : 0,'det' : 0, 'denna': 0, 'denne' : 0, 'hen
 
 import urllib2
 response = urllib2.urlopen('http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_0.txt')
-html = response.read()
+#html = response.read()
 
-with open(html) as f:
-    for row in f:
-        if (row == ''):
-            break 
-        if (row == '\n'):
-            continue 
-        json2 = json.loads(row)
-        if ('retweeted_status'  in json2):
-            skip = skip + 1
-            continue
-        text = json2['text']
-        for word in words:
-            n = text.count(word)
-            amount = words.get(word)
-            words[word] = n + amount
+#with open(html) as f:
+for row in response:
+    if (row == ''):
+        break 
+    if (row == '\n'):
+        continue 
+    json2 = json.loads(row)
+    if ('retweeted_status'  in json2):
+        skip = skip + 1
+        continue
+    text = json2['text']
+    for word in words:
+        n = text.count(word)
+        amount = words.get(word)
+        words[word] = n + amount
 for word in words:
     print('ordet ' + word + " antalet " + str(words[word]))  
 print('skip ' + str(skip))     
